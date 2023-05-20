6af5ba37cea6adbb8d97d3829d21aba760002a73
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-05-20 23:11:18 +0200

    Added the photos and a new App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Importing the style file
+import "./App.css";
+
+// Creating the App component
+const App = () => {
+  return <div className="app-container"></div>;
+};
+
+// Exporting the App component
+export default App;
