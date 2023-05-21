574a2afbbb440f61861eb455228398b711b01cd1
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-05-21 16:42:09 +0200

    Updates

---
+++ precache-manifest.40a2e6162931e5e863da6a9dd180d803.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48367d5d23319fef9708",
+    "url": "/parallax-site/static/css/main.25f6bee2.chunk.css"
+  },
+  {
+    "revision": "48367d5d23319fef9708",
+    "url": "/parallax-site/static/js/main.48367d5d.chunk.js"
+  },
+  {
+    "revision": "aaa46b8168ad4f460655",
+    "url": "/parallax-site/static/js/1.aaa46b81.chunk.js"
+  },
+  {
+    "revision": "9b4b3806ab68dabce473",
+    "url": "/parallax-site/static/js/runtime~main.9b4b3806.js"
+  },
+  {
+    "revision": "cc093e9d78a02bc599bbcb9a151b2958",
+    "url": "/parallax-site/static/media/earth_bg.cc093e9d.jpg"
+  },
+  {
+    "revision": "b64977276ee6f6d305a63af45da0c145",
+    "url": "/parallax-site/static/media/earth.b6497727.png"
+  },
+  {
+    "revision": "a868fa516e386a1b4a2a9e5d13a13f82",
+    "url": "/parallax-site/static/media/earth2_bg.a868fa51.jpg"
+  },
+  {
+    "revision": "0f8702d24826f38220c70fc132d129be",
+    "url": "/parallax-site/static/media/earth2.0f8702d2.png"
+  },
+  {
+    "revision": "44709a76a46177d6ee78bd564868b220",
+    "url": "/parallax-site/index.html"
+  }
+];
